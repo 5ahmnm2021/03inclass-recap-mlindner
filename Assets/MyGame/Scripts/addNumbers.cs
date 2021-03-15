2a3731e17252835f25b7d75ffee8704cdc7e80f9
yslindner <marco.lindner@htl-salzburg.ac.at>
2021-03-15 15:24:07 +0100

    Created addNumbers Script, defined variables.

---
+++ Assets/MyGame/Scripts/addNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Adding Numbers Calculation Marco Lindner
+
+public class addNumbers : MonoBehaviour
+{
+    public InputField num1_in;
+    public InputField num2_in;
+    public float num1;
+    public float num2;
+    public Text result;
+    bool correctIn1 = false;
+    public bool correctIn2 = false;
+
+}
