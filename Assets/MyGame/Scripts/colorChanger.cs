3f662450a48cc5ff3e3ea04752237c0f2452b098
yslindner <marco.lindner@htl-salzburg.ac.at>
2021-03-15 15:20:09 +0100

    Important Scenes from last Project.

---
+++ Assets/MyGame/Scripts/colorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Changes the Color of the Square when Space is clicked.
+
+// Colors from Adobe Color: https://color.adobe.com/create/color-wheel
+
+public class colorChanger : MonoBehaviour
+{
+    public GameObject color;
+    
+// Defining Colors
+    public void ChangeColor()
+    {
+        Color[] colors =
+        {
+            new Color32(35,123,173,68),
+            new Color32(250,100,105,98),
+            new Color32(75,186,250,98),
+            new Color32(250,246,50,98),
+            new Color32(173,171,43,68),
+        };
+
+        if (Input.GetKeyDown("space"))
+        {
+            color.GetComponent<Image>().color = colors[Random.Range(0, colors.Length)];
+        }
+    }
+
+    void Update()
+    {
+        ChangeColor();
+    }
+}
---
+++ Assets/MyGame/Scripts/sceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Scene Loader Marco Lindner
+
+public class sceneLoader : MonoBehaviour
+{
+    public void LoadColorScene()
+    {
+        SceneManager.LoadScene("01colorScene");
+    }
+
+    public void LoadNumberScene()
+    {
+        SceneManager.LoadScene("02numberScene");
+    }
+
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene("00welcomeScene");
+    }
+}
